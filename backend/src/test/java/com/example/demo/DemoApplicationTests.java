b3023a8e887e94ca0c4ea81c7b174d69fd5bad89
heyz-hye <dull.win@yahoo.com>
2026-03-28 14:52:44 -0400

    [hey]

---
+++ backend/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import com.com.yourproject.communityapp.CommunityAppApplication;
+
+@SpringBootTest(classes = CommunityAppApplication.class)
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
