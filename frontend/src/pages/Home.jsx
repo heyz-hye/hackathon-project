4595aa6064cbaa8b31413e5937d75c8f1bb640e3
chrisp4713 <chrisp4713@gmail.com>
2026-03-28 12:12:24 -0400

    started working on home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,7 +4,19 @@ import Navbar from '../components/Navbar.jsx'
 function Home(){
     return (
         <>
-        <h1>hi</h1>
+        <Navbar/>
+        <h1>Welcome to website</h1>
+        <div>
+            <h2>Pantries</h2>
+
+        </div>
+        <div>
+            <h2>Libraries</h2>
+
+        </div>
+        <div>
+            <h2>Places to rent</h2> 
+        </div>
         </>
     )
 }
