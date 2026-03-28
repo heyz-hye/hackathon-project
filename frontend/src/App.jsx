5d845902f3b621d2ff1cec7f9d4e1c7e67360603
chrisp4713 <chrisp4713@gmail.com>
2026-03-28 11:53:12 -0400

    set up navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import Home from './pages/Home.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-     
+      <Home/> 
     </>
   )
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,12 @@
-import 'Home.css'
+import './Home.css'
 import Navbar from '../components/Navbar.jsx'
 
 function Home(){
-    <>
-
-    </>
+    return (
+        <>
+        <h1>hi</h1>
+        </>
+    )
 }
 
 export default Home
